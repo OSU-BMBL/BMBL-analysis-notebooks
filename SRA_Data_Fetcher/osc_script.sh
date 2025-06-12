616e5276e7fabdfda85248ebe88ecda97fca7c00
shaohong feng <feng1426@ascend-rw01.ten.osc.edu>
2025-06-12 15:18:35 -0400

    upgrade python and sratoolkit version

--- SRA_Data_Fetcher/osc_script.sh
+++ SRA_Data_Fetcher/osc_script.sh
@@ -7,9 +7,9 @@
 #SBATCH --mem=128GB
 
 date
-module load python/3.9-2022.05
-module load sratoolkit/2.11.2
-
-cd $HOME
+module load python/3.12
+module load sratoolkit/3.0.2
+# cp ./fetch-with-sratoolkit.py $HOME
+# cd $HOME
 python ./fetch-with-sratoolkit.py -o ./ -t ./ ./sra_ids.json
 date
