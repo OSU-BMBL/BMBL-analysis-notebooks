3d10563212927a52a6fa4d082539f2d1f000daf0
Hu Chen <hu.chen@osumc.edu>
2025-06-19 10:32:13 -0400

    pipline test - Hu

--- scRNAseq_Seurat_to_Scanpy/seurat_to_scanpy.R
+++ scRNAseq_Seurat_to_Scanpy/seurat_to_scanpy.R
@@ -12,16 +12,19 @@ set.seed(42)
 # Query data
 sample_name <- "combined.qsave"
 combined <- qs::qread(sample_name)
-DefaultAssay(combined) <- "RNA"
-
 combined <- UpdateSeuratObject(combined)
+
+DefaultAssay(combined) <- "RNA"
+# or 
+DefaultAssay(seu) <- "SCT"
+
 #combined@assays$RNA@scale.data <- matrix() # Optional: clean scaled data
 #combined@assays$RNA@data <- matrix() # Optional: clean normalized data 
 
 #combined@meta.data <- combined@meta.data[, 1:10] # Some metadata are all NAs, causing errors while loading in Scanpy
 SaveH5Seurat(combined,
-             filename = paste0(sample_name, ".h5Seurat"),
+             filename = "combined.h5Seurat",
              overwrite = T)
-Convert(paste0(sample_name, ".h5Seurat"),
+Convert("combined.h5Seurat",
         dest = "h5ad",
         overwrite = T)
